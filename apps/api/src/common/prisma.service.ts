da091c7e9db1e45d2607659aaa49006172f9a20a
Jay Kushwah <jaykush.work@gmail.com>
2026-06-25 13:45:09 +0530

    fix prisma service typing on vercel

--- apps/api/src/common/prisma.service.ts
+++ apps/api/src/common/prisma.service.ts
@@ -1,19 +1,54 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+// import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+//   private readonly logger = new Logger(PrismaService.name);
+
+//   constructor() {
+//     super({
+//       log: ['warn', 'error'],
+//     });
+//   }
+
+//   async onModuleInit() {
+//     await this.$connect();
+//     this.logger.log('✓ Prisma connected to database');
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from "@nestjs/common";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaClient } = require("@prisma/client");
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  [key: string]: any;
+
   private readonly logger = new Logger(PrismaService.name);
 
   constructor() {
     super({
-      log: ['warn', 'error'],
+      log: ["warn", "error"],
     });
   }
 
   async onModuleInit() {
     await this.$connect();
-    this.logger.log('✓ Prisma connected to database');
+    this.logger.log("✓ Prisma connected to database");
   }
 
   async onModuleDestroy() {
